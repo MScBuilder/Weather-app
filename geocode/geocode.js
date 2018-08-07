84611d87fdaad33a64f97bb9c7bd22ac849f1e38
Dariusz Lyssy <darek.lyssy@gmail.com>
2018-08-07 20:07:31 +0200

    Działa bez "obietnic"

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,15 +1,18 @@
 const request = require('request');
 
-const geocodeAddress = (address, callback, ) => {
+const geocodeAddress = (address, callback) => {
     let encodedAddress = encodeURIComponent(address);
     request({
-        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyDrVqk_N8cQl8P6Xdkcj1ve-6kTHEUoAK4`,
+          // url: `https://maps.googleapis.com/maps/api/geocode/json?address=19146`,
         json: true
     }, (error, response, body) => {
         if (error) {
             callback('Unable to connecto to google servers.');
         } else if (body.status === 'ZERO_RESULTS') {
             callback('Unable to find that address');
+        } else if (body.status === 'OVER_QUERY_LIMIT'){
+            callback('You reach max query limit in Google geocde API');
         } else if (body.status === 'OK') {
             callback(undefined, {
                 address: body.results[0].formatted_address,
@@ -17,7 +20,7 @@ const geocodeAddress = (address, callback, ) => {
                 longitude: body.results[0].geometry.location.lng
             });
         } else {
-            console.log('Somethings goes wrong!\nPlease, check if you use correct option');  
+            console.log('Somethings goes wrong!\nPlease, check if you use correct option');
         }
     });
 }
---
+++ weather/weather.js
@@ -0,0 +1,26 @@
+const request = require('request');
+
+const currentWeather = (latitude, longitude, callback) => {
+  request({
+    url: `https://api.darksky.net/forecast/5f7a2c3b5c552513b2c986df723b0d6e/${latitude},${longitude}`,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      callback('Unable to reach Forecast.io servers');
+    } else if (body.code === 400) {
+      callback(body.error);
+    } else if (body.latitude) {
+      callback(undefined, {
+        weather_summary: body.currently.summary,
+        temperature: body.currently.temperature,
+        aparent_temperature: body.currently.apparentTemperature,
+        pressure: body.currently.pressure,
+        humidity: body.currently.humidity,
+      })
+    } else {
+      console.log('Please, check if your request was correct!')
+    };
+  });
+};
+
+module.exports.currentWeather = currentWeather;
