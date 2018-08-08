77ad2e90a83767cfeabe3bd551be8989c376c527
Dariusz Lyssy <darek.lyssy@gmail.com>
2018-08-08 20:35:31 +0200

    Dodano opcję domyślnego adresu

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const yargs = require('yargs');
+const fs = require('fs');
 const geocode = require('./geocode/geocode');
 const weather = require('./weather/weather');
 
 const argv = yargs
+    .command(['default [address]', 'd', 'def'], 'Set used addres as default', {}, (argv) => {
+      fs.writeFileSync('defaultAddress.txt', argv.address);
+      console.log('You use defaultAddress command!');
+    })
     .options({
         a: {
-            demand: true,
+            demand: false,
             alias: 'address',
             describe: 'Address to fetch weather for',
             string: true
+        },
+        d: {
+          demand: false,
+          alias: 'defaultAddress',
+          describe: 'Setting your default address',
+          string: true
         }
     })
     .help()
     .alias('help', 'h')
     .argv;
 
-geocode.geocodeAddress(argv.address).then((location) => {
+let addressToFetch = argv.address;
+
+if (argv.address === undefined) {
+  addressToFetch = fs.readFileSync('defaultAddress.txt');
+}
+
+geocode.geocodeAddress(addressToFetch).then((location) => {
   console.log (JSON.stringify(location, undefined, 2));
   weather.currentWeather(location.latitude, location.longitude, (errorMessage, weatherResults) => {
     if (errorMessage) {
